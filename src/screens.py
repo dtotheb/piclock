b1ba6b164968e96fc12ac65dfb5fc10ef69ef259
dan branton <dtotheb@gmail.com>
2016-06-19 18:54:26 -0400

    improved status screen layout & colored buttons

--- src/screens.py
+++ src/screens.py
@@ -9,10 +9,29 @@ import time
 from subprocess import *
 
 ip_cmd = "ip addr show wlan0 | grep -w inet | awk '{print $2}' | cut -d/ -f1"
+cputemp_cmd = 'vcgencmd measure_temp'
+uptime_cmd = 'uptime -p'
+
+
 def run_cmd(cmd):
     p = Popen(cmd, shell=True, stdout=PIPE)
-    output = p.communicate()[0]
+    output = p.communicate()[0].rstrip()
     return output
+
+
+def getIPaddress():
+    return run_cmd(ip_cmd)
+
+
+
+def getCPUtemperature():
+    res = run_cmd(cputemp_cmd)
+    return(res.replace("temp=","").replace("'C\n",""))
+
+def getUptime():
+    res = run_cmd(uptime_cmd)
+    return(res.replace("up ",""))
+
 
 
 class ScreenManagement(ScreenManager):
@@ -27,13 +46,22 @@ class WeatherScreen(Screen):
 class ClockScreen(Screen):
     current_time = ObjectProperty()
 
+    def setupClockScreen(self):
+        Clock.schedule_interval(self.updateTime, 0.01)
+
     def updateTime(self, dt):
         self.current_time.text = time.strftime("%I:%M:%S", time.localtime())
 
 class StatusScreen(Screen):
-    ip_address = ObjectProperty()
+    ip = ObjectProperty()
+    cuptemp = ObjectProperty()
+    uptime = ObjectProperty()
 
+    def setupStatusScreen(self):
+        self.updateStatus(0.1)
+        Clock.schedule_interval(self.updateStatus, 1)
 
     def updateStatus(self, dt):
-        #self.ip_address.text = run_cmd(ip_cmd)
-        pass
+        self.ip.text = getIPaddress()
+        self.cputemp.text = getCPUtemperature()
+        self.uptime.text = getUptime()
